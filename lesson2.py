075f66696d8a9734b9a987d11e2c50582d7f95af
Brandon Presley <brandon@presley.nz>
2024-10-02 13:36:39 +1300

    Add lesson 2

---
+++ lesson2.py
@@ -0,0 +1,8 @@
+# Lesson 2 - Loops & Lists
+
+from game import run_game
+
+speeds = [5, 10, 20]
+
+for speed in speeds:
+    run_game(log_speed=speed)
---
+++ quiz2.py
@@ -0,0 +1,15 @@
+people = ['Megan', 'Brandon', 'John']
+ages = [21, 26, 30]
+
+# What names will be printed?
+for person in people:
+    print('Found person:', person)
+    
+# What ages will be printed?
+for age in ages:
+    print('Found age:', age)
+    
+ages.append(18)
+print(ages)
+
+# What does ages look like now?
