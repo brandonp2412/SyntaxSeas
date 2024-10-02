ffff9949029784aed58985a29ecf3ba5a739e0cf
Brandon Presley <brandon@presley.nz>
2024-10-02 13:52:16 +1300

    Add quiz 3

---
+++ quiz3.py
@@ -0,0 +1,22 @@
+# Addition quiz - What's the result?
+
+snakes = 3
+camels = 5
+
+print('We have:', snakes + camels, 'total animals (Addition quiz)')
+
+# Multiplication quiz - What's the result?
+
+width = 100
+height = 2
+
+print('Our floor is:', width * height, 'square meters (Multiplication quiz)')
+
+# Division quiz - What's the result?
+
+weekends = 2
+year = 365
+days = 7
+
+print('There are:', year / days * weekends, 'weekends in a year (BEDMAS quiz)')
+
