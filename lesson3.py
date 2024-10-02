8e5b59471ea1fb3233a2f4f7b7f7c0512c9a418c
Brandon Presley <brandon@presley.nz>
2024-10-02 13:47:14 +1300

    Add division & floor to lesson 3

--- lesson3.py
+++ lesson3.py
@@ -3,6 +3,7 @@
 hours = 24
 days = 7
 months = 12
+year = 365
 
 # Calculate how many hours are in a week
 
@@ -14,3 +15,11 @@ print('There are:', week, 'hours in a week (loop)')
 # How can we simplify this?
 
 print('There are:', days * hours, 'hours in a week (multiplication)')
+
+# How many weeks are there in a year?
+
+print('There are:', year / days, 'weeks in a year (division)')
+
+# Let's shorten that number...
+
+print('There are:', year // days, 'weeks in a year (division)')
