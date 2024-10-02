3cf11b2bc87e72e8df4852d32039c860beee1750
Brandon Presley <brandon@presley.nz>
2024-10-02 13:31:08 +1300

    Use addition in 1st quiz

--- quiz1.py
+++ quiz1.py
@@ -6,3 +6,11 @@ age = 20
 
 # What type is age now?
 age = '20'
+
+# What happens when you add numbers together?
+
+print('One plus one equals:', 1 + 1)
+
+# How about adding numbers to words?
+
+print('One plus one equals:' + 2)
