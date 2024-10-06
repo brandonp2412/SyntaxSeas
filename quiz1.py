9df342c5f65713c35ec0f19bfa05de0d37f256b1
Brandon Presley <brandon@presley.nz>
2024-10-06 14:40:52 +1300

    Add escaping and string interpolation to quiz 1

--- quiz1.py
+++ quiz1.py
@@ -1,5 +1,5 @@
 # What type is name?
-name = 'Ahoy matey!'
+statement = 'Ahoy matey!'
 
 # What type is age?
 age = 20
@@ -14,3 +14,11 @@ print('One plus one equals:', 1 + 1)
 # How about adding numbers to words?
 
 print('One plus one equals:' + 2)
+
+# How do we write quotes in our strings?
+
+print('Aren\'t you a sly one?')
+
+# Better ways to build sentences
+
+print(f'Im {age} and i say {statement}')
